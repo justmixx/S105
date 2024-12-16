4918f25702b3a7c5af6e570511f0cd1574c1b2c2
jusmixx <justin.gonnet@gmail.com>
2024-12-16 15:21:14 +0100

    Ajout de fichiers index et stylesheet

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$titre='TITRE';
+?>
+<html>
+    <head>
+        <title><?php
+        echo $titre;?></title>
+    </head>
+    <body>
+        <h1><?php
+        echo $titre;?>
+    </body>

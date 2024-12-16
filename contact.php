7e3a539fbd56c829aaca8b0e63941f177dfc84b2
jusmixx <justin.gonnet@gmail.com>
2024-12-16 15:29:45 +0100

    Ajout fichier contact

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+$titre='Contact';
+?>
+<html>
+    <head>
+        <title><?php
+        echo $titre;?></title>
+    </head>
+    <body>
+        <h1><?php
+        echo $titre;?>
+    </body>

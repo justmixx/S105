afb70fd6cf7402f979899a5a885a0fb98953a6d9
jusmixx <justin.gonnet@gmail.com>
2025-01-17 14:34:36 +0100

    header

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,7 @@
-<nav>
+<footer>
             <ul>
                 <li><a href="contact.php">Contact</a></li>
                 <li><p>&copy; 2024 Mouches de Combat</p></li>
                 <li><a href="mentions-legales.php">Mentions légales</a></li>
             </ul>
-        </nav>
+</footer>
--- include/header.php
+++ include/header.php
@@ -1,25 +1,37 @@
 <nav>
-            <div class="nav-container">
-                <a href="index.php" class="logo">
-                    <img src="img/logo.svg" alt="Logo Mouches de Combat">
-                </a>
-                <div class="burger">
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-                <ul class="nav-links">
-                    <li><a href="catalogue.php">Catalogue</a></li>
-                    <li><a href="arene.php">Arène</a></li>
-                    <li><a href="vendre.php">Vendre</a></li>
-                    <li><a href="laboratoire.php">Notre laboratoire</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                </ul>
-                <div class="cart-link">
-                    <a class="button" href="panier.php">
-                        <img src="img/fleches/panier.png" alt="icon panier">
-                        mon panier
-                    </a>
-                </div>
-            </div>
-        </nav>
+    <div class="nav-container">
+        <a href="index.php" class="logo">
+            <img src="img/logo.svg" alt="Logo Mouches de Combat">
+        </a>
+        <div class="burger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        <ul class="nav-links">
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="catalogue.php">Catalogue</a></li>
+            <li><a href="arene.php">Arène</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        <div class="cart-link">
+            <a class="button" href="panier.php">
+                <img src="img/fleches/panier.png" alt="icon panier">
+                mon panier
+            </a>
+        </div>
+    </div>
+</nav>
+<script>
+    // Sélectionner l'élément 'nav'
+    const nav = document.querySelector('nav');
+
+    // Détecter le défilement de la page
+    window.onscroll = function() {
+        if (window.scrollY > 50) { // Si la page est défilée de plus de 50px
+            nav.classList.add('shrink'); // Ajouter la classe 'shrink'
+        } else {
+            nav.classList.remove('shrink'); // Supprimer la classe 'shrink'
+        }
+    };
+</script>

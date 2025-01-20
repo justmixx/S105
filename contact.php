8026c89b8767c7da91b3300deb2dd2165f68a38f
alexiszehr <alexis.zehr@azdesign.ovh>
2025-01-20 11:02:01 +0100

    rgrerg

--- contact.php
+++ contact.php
@@ -39,13 +39,13 @@
                 <h3>Suivez-nous :</h3>
                 <div class="social-icons">
                     <a href="https://facebook.com" target="_blank">
-                        <img src="/Applications/MAMP/htdocs/S105/img/logo/facebook.png" alt="Facebook">
+                        <img src="img/logo/facebook.png" alt="Facebook">
                     </a>
                     <a href="https://instagram.com" target="_blank">
-                        <img src="/Applications/MAMP/htdocs/S105/img/logo/instagram.ico" alt="Instagram">
+                        <img src="img/logo/instagram.ico" alt="Instagram">
                     </a>
                     <a href="https://twitter.com" target="_blank">
-                        <img src="../img/twitter-icon.svg" alt="Twitter">
+                        <img src="img/twitter-icon.svg" alt="Twitter">
                     </a>
                 </div>
             </div>

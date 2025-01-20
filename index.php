72424ade023027c46aad7611a68c0b2b41bc7f04
jusmixx <justin.gonnet@gmail.com>
2025-01-20 09:05:21 +0100

    rgbdfg

--- index.php
+++ index.php
@@ -53,11 +53,7 @@ $titre='TITRE';
         </section>
 
         <!-- Deuxième section à droite de la première -->
-        <section>
-            <h2>Les combats de mouches accessibles facilement.</h2>
-            <p class="text">Avec notre système de combat, il n'a jamais été aussi simple de combattre.</p>
-            <a class="bouton button" href="arene.php">Entrez dans l'arène</a>
-        </section>
+        <iframe src="https://www.youtube.com/embed/PL3Odw-k8W4?si=Vw31byHBnJZfvxnj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 
     <!-- Bloc 2 sous les sections -->
